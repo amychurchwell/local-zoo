f5e46585787da2bb898ef92220cd2ce21cc553c1
Amy Churchwell <amy.churchwell@gmail.com>
2018-01-19 09:52:40 -0800

    debug animal array display

--- src/app/animal.component.ts
+++ src/app/animal.component.ts
@@ -4,16 +4,12 @@ import { Component } from '@angular/core';
  selector: 'animal',
  templateUrl: './animal.component.html'
 })
+
 export class Animal {
- constructor(
-   public species: string,
-   public name: string,
-   public age: number,
-   public diet: string,
-   public location: string,
-   public caretakers: number,
-   public sex: string,
-   public likes: string,
-   public dislikes: string,
- ){}
+  public pintsRemaining: number = 124;
+  constructor(
+    public species: string,
+    public name: string,
+    public age: number,
+  ){ }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,14 @@ import { Animal } from './animal.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Animal Tracker';
   animals: Animal[] = [
-    new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud Noises')
-  ];
-  constructor() {}
+    new Animal('panda', 'oreo', 3),
+    new Animal('dog', 'Tala', 2)
+  ]
 }
+
+// new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud Noises'),
+// new Animal('Panda', 'Oreo', 5, 'Omnivore', 'China', 8, 'Male', 'Bamboo', 'Data Entry')
